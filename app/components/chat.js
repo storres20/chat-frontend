c11ae86f1266a7aa8da39a8316cce0e76637af83
storres20 <sanchotorres777@gmail.com>
2024-10-18 03:00:44 -0500

    Update vps ip

--- app/components/chat.js
+++ app/components/chat.js
@@ -13,8 +13,8 @@ const Chat = () => {
     const messagesEndRef = useRef(null); // Ref to track the end of the messages
 
     useEffect(() => {
-        //const socket = new WebSocket('ws://145.223.120.127:3001'); // Replace with your VPS IP
-        const socket = new WebSocket('ws://192.168.1.49:3001'); // Replace with your VPS IP
+        const socket = new WebSocket('ws://145.223.120.127:3001'); // Replace with your VPS IP
+        //const socket = new WebSocket('ws://192.168.1.49:3001'); // Replace with your VPS IP
         setWs(socket);
 
         socket.onopen = () => {
