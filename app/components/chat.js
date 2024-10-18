40fcc44dbf2c97fff37b5a117dfe4950d013dd04
storres20 <sanchotorres777@gmail.com>
2024-10-18 03:56:26 -0500

    Add subdomain for wss

--- app/components/chat.js
+++ app/components/chat.js
@@ -13,8 +13,10 @@ const Chat = () => {
     const messagesEndRef = useRef(null); // Ref to track the end of the messages
 
     useEffect(() => {
-        const socket = new WebSocket('ws://145.223.120.127:3001'); // Replace with your VPS IP
-        //const socket = new WebSocket('ws://192.168.1.49:3001'); // Replace with your VPS IP
+        //const socket = new WebSocket('ws://145.223.120.127:3001'); // Replace with your VPS IP
+        //const socket = new WebSocket('ws://192.168.1.49:3001'); // Replace with your localhost IP
+        const socket = new WebSocket('wss://chat.lonkansoft.pro:3001'); // Secure WebSocket connection with WSS
+
         setWs(socket);
 
         socket.onopen = () => {
